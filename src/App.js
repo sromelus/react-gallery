047a0e093ed22b056ee504e336219128a98acc7a
sromelus <46148909+sromelus@users.noreply.github.com>
2020-02-28 02:33:02 -0500

    add handleTagChange function in PhotoContainer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SearchForm from './components/SearchForm';
 import Nav from './components/Nav';
 import PhotoContainer from './components/PhotoContainer';
@@ -8,20 +8,17 @@ import NotFound from './components/NotFound';
 
 class App extends Component {
 
-
-
-
-
-
 render(){
-  console.log(this.props);
   return (
     <BrowserRouter>
       <div className="container">
         <SearchForm />
         <Nav />
         <Switch>
-          <Route path="/" component={ PhotoContainer} />
+          <Route exact path="/" component={ PhotoContainer} />
+          <Route path="/dogs" key='1' component={ PhotoContainer} />
+          <Route path="/cars" key='2' component={ PhotoContainer} />
+          <Route path="/planes" key='3' component={ PhotoContainer} />
           <Route component={ NotFound } />
         </Switch>
       </div>
@@ -31,18 +28,3 @@ render(){
 }
 
 export default App;
-
-
-// <Route path="/cats"  component={ () => <PhotoContainer tag='cats '/> } />
-// <Route path="/dogs"  component={ () => <PhotoContainer tag='dogs' /> } />
-// <Route path="/computers"  component={ () => <PhotoContainer tag='computers' /> } />
-
-// <Route exact path="/"  render={() =>  <Redirect to='/cats' /> } />
-// <Route path="/cats"  component={ PhotoContainer} />
-// <Route path="/dogs"  component={ () => <PhotoContainer tag='dogs' /> } />
-// <Route path="/computers"  component={ () => <PhotoContainer tag='computers' /> } />
-
-// <Route exact path="/"  render={() =>  <Redirect to='/cats' /> } />
-// <Route path="/cats" key='1' component={ PhotoContainer} />
-// <Route path="/cars" key='2' component={ PhotoContainer} />
-// <Route path="/planes" key='3' component={ PhotoContainer} />
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -13,13 +13,29 @@ class PhotoContainer extends Component {
    };
   }
 
+  handleTagChange(props) {
+    let tag = ' '
+    const searchQuery = props.location.search;
+    const querySearchValue = queryString.parse(props.location.search).search;
+    const queryPathValue = props.match.url;
+
+    if(queryPathValue === "/" && querySearchValue){
+      tag = querySearchValue;
+    } else if (queryPathValue.length > 0 && querySearchValue) {
+      props.history.push(`/${searchQuery}`)
+    } else if (queryPathValue === "/")  {
+      this.props.history.push("/dogs");
+    } else {
+      tag = queryPathValue.replace("/", "");
+    }
+    return tag;
+  }
+
   componentDidMount(){
-    // const tag = this.props.match.url.replace('/', '')
-    // ${tag}
-    const queryValue = queryString.parse(this.props.location.search)
-    console.log(this.props.location.search);
-    console.log(queryValue.search);
-    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&tags=''&per_page=24&format=json&nojsoncallback=1`)
+
+  const currentTag = this.handleTagChange(this.props)
+
+    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&tags=${currentTag}&per_page=24&format=json&nojsoncallback=1`)
       .then(response => response.json())
       .then(body => body.photos.photo)
       .then(photo => {
